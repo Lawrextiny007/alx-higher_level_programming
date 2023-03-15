0e34dccc76a39d1a91d6334f6810291f88ca4b93
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2023-03-15 11:03:45 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 0-print_list_integer.py
-
-
-def print_list_integer(my_list=[]):
-    """Print all integers of a list."""
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def square_matrix_simple(matrix=[]):
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
